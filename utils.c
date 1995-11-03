c2b24cb84483dfc731ca9dfdb484530e0689efe2
jonathan <jonathan@4199a21b-042e-0410-bf9e-f94d0c2d158d>
1995-11-03 14:34:04 +0000

    whoops! forgot to add these new files.
    git-svn-id: file:///var/svn/repos/usepackage/trunk@49 4199a21b-042e-0410-bf9e-f94d0c2d158d

---
+++ utils.c
@@ -0,0 +1,61 @@
+
+/* utils.c */
+
+/* Jonathan AH Hogg */
+
+
+/*** uses: ***/
+
+#include <pwd.h>
+#include <string.h>
+#include "utils.h"
+
+
+/*** functions: ***/
+
+int is_csh_user(void)
+{
+   struct passwd *pwent;
+   char *shell;
+
+   pwent = getpwuid(getuid());
+   shell = strrchr(pwent->pw_shell, '/');
+   if (!shell) return(0);
+   shell++;
+
+   return ((!strcmp(shell, "csh")) || (!strcmp(shell, "tcsh")));
+}
+
+
+char* expand(char* filepath)
+{
+   static char newpath[256];
+   static char username[32];
+   struct passwd* pwent;
+   int i,j;
+
+   i = 0;
+   newpath[0] = '\0';
+
+   if (filepath[i] == '~')
+   {
+      i++;
+
+      if (filepath[i] == '/')
+	 pwent = getpwuid(getuid());
+      else
+      {
+         j = i;
+         while (filepath[i] && (filepath[i] != '/')) i++;
+         strncpy(username, filepath + j, i - j);
+	 pwent = getpwnam(username);
+      }
+
+      strcat(newpath, pwent->pw_dir);
+   }
+
+   strcat(newpath, filepath + i);
+
+   return(newpath);
+}
+
---
+++ utils.h
@@ -0,0 +1,10 @@
+
+/* utils.h */
+
+
+/*** prototypes: ***/
+
+int is_csh_user(void);
+char* expand(char* filepath);
+
+
